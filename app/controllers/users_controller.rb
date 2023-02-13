8ea635a6f74ec3794e8dc0a9ca4fbe26d9e145c0
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-02-13 16:45:37 -0500

    Fix linters error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
- load_and_authorize_resource :class => UsersController
+  load_and_authorize_resource class: UsersController
 
- def index
-  @users = User.all
- end
+  def index
+    @users = User.all
+  end
 end
