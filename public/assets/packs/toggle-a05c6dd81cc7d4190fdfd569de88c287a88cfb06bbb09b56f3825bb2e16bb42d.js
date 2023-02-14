f370698cdfda58a8263a9b09192d437866029cc6
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-02-14 01:03:27 -0500

    Fix linters

--- public/assets/packs/toggle-a05c6dd81cc7d4190fdfd569de88c287a88cfb06bbb09b56f3825bb2e16bb42d.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('navbar-default');
-const btn = document.getElementById('btn-toggle');
-
-btn.addEventListener('click', () => {
-  toggle.classList.toggle('show');
-});
